e3fd8ddc97fd53a5f07eb3af743463978c7bb868
root <root@localhost>
2021-09-28 00:56:44 +0000

    Reversed flash code mistake

--- spec/feature/date_integration_spec.rb
+++ spec/feature/date_integration_spec.rb
@@ -7,9 +7,9 @@ RSpec.describe 'Creating a book', type: :feature do
     fill_in 'Title', with: 'harry potter'
     fill_in 'Author', with: 'Jk Rowling'
     fill_in 'Price', with: 10.56
-    select'01', from: 'book[published_date(1i)]'
+    select'01', from: 'book[published_date(3i)]'
     select '01', from: 'book[published_date(2i)]'
-    select '2011', from: 'book[published_date(3i)]'
+    select '2011', from: 'book[published_date(1i)]'
     click_on 'Submit'
     expect(page.status_code).to eq(200)
   end
