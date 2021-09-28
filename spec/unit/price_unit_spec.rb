878d78908225bc92876110bab0b48d8a721ccf49
root <root@localhost>
2021-09-28 01:35:27 +0000

    ALL unit and intergation tests pass

--- spec/unit/price_unit_spec.rb
+++ spec/unit/price_unit_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 
 RSpec.describe Book, type: :model do
   subject do
-    described_class.new(price: 10.5)
+    described_class.new(title: 'harry potter', author: 'JK Rowling', 
+    price: 10.5, published_date: '01/01/2011')
   end
 
   it 'is valid with valid attributes' do
@@ -11,7 +12,7 @@ RSpec.describe Book, type: :model do
   end
 
   it 'is not valid without a value' do
-    subject.price = 0
+    subject.price = nil
     expect(subject).not_to be_valid
   end
 end
